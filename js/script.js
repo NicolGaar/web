ada9b1043d1360b004f21ec101864b29fcdcf43a
nicogaar <nicol.gaar@gmail.com>
2023-07-13 17:40:53 -0400

    trece julio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $('.carousel').carousel()
 
+$('#carouselExampleControls').carousel({
+    interval: 3000, // Tiempo de transición entre diapositivas en milisegundos (opcional)
+    wrap: true, // Permite el bucle continuo del carrusel (opcional)
+  });
   
