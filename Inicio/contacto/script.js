8dab0f6f713f9871bbb53b7e8c8e41639c88bbe8
NicoMoraga~ <nicomora1998@gmail.com>
2023-07-25 19:20:21 -0400

    Se agrega contacto

--- Inicio/contacto/script.js
+++ Inicio/contacto/script.js
@@ -9,6 +9,33 @@ document.addEventListener('DOMContentLoaded', function() {
       form.classList.add('was-validated');
     }, false);
   });
+
+
+  function zoomIn(element) {
+    element.style.fontSize = "20px"; // Tamaño de fuente aumentado (puedes ajustar este valor)
+  }
+  
+  function zoomOut(element) {
+    element.style.fontSize = "16px"; // Tamaño de fuente original
+  }
+
+/*API de mapa*/
+function inicializarMapa() {
+  // Código para inicializar el mapa aquí
+  // Por ejemplo:
+  var ubicacion = { lat: -33.39789040176534, lng: -70.75083327420944}; // Latitud y longitud de la ubicación deseada
+  var mapa = new google.maps.Map(document.getElementById('mapa'), {
+    zoom: 16, // Nivel de zoom inicial
+    center: ubicacion // Centrar el mapa en la ubicación especificada
+  });
+
+  // Puedes agregar marcadores, polígonos, etc., según tus necesidades
+  var marcador = new google.maps.Marker({
+    position: ubicacion,
+    map: mapa,
+    title: '¡Estoy aquí!'
+  });
+}
   
 
 
